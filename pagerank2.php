5dc2391707b8ec37cc63e41ab6a2e761059e0bd5
James Wade <web@hm2k.org>
2012-05-15 22:17:27 +0100

    Fix a tabbing issue

--- pagerank2.php
+++ pagerank2.php
@@ -19,7 +19,7 @@ function GetPageRank($q,$host='toolbarqueries.google.com',$context=NULL) {
 		$result ^= ord($seed{$i%strlen($seed)}) ^ ord($q{$i});
 		$result = (($result >> 23) & 0x1ff) | $result << 9;
 	}
-   if (PHP_INT_MAX != 2147483647) { $result = -(~($result & 0xFFFFFFFF) + 1); }
+    if (PHP_INT_MAX != 2147483647) { $result = -(~($result & 0xFFFFFFFF) + 1); }
 	$ch=sprintf('8%x', $result);
 	$url='http://%s/tbr?client=navclient-auto&ch=%s&features=Rank&q=info:%s';
 	$url=sprintf($url,$host,$ch,$q);
